f3e2bbe98e2ebb097f320b6750a8fd15f7363b45
Chitcharus <NCT@AP.XOM.com>
2022-03-13 19:51:38 +0700

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -92,7 +92,7 @@ export const protectedResources = {
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
  export const loginRequest = {
-    scopes: ["openid",...protectedResources.apiHello.scopes]
+    scopes: [...protectedResources.apiHello.scopes]
 };
 
 const tokenRequest = {
